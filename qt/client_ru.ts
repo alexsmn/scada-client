77c7e25928ef3f162de899d45a6c741b23a0b45c
alexsmn <alexsmn@gmail.com>
2018-05-27 07:19:23 -0600

    client-qt: Added translations.

---
+++ qt/client_ru.ts
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>LoginDialog</name>
+    <message>
+        <source>Login</source>
+        <translation>Вход в систему</translation>
+    </message>
+    <message>
+        <source>User name:</source>
+        <translation>Имя:</translation>
+    </message>
+    <message>
+        <source>Password:</source>
+        <translation>Пароль:</translation>
+    </message>
+    <message>
+        <source>Auto-Login:</source>
+        <translation>Автоматически:</translation>
+    </message>
+    <message>
+        <source>Server:</source>
+        <translation>Сервер:</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindowQt</name>
+    <message>
+        <source>Telecontrol SCADA Client</source>
+        <translation>Клиент ОИК Телеконтроль</translation>
+    </message>
+    <message>
+        <source>Toolbar</source>
+        <translation>Панель инструментов</translation>
+    </message>
+</context>
+</TS>
