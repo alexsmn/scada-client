ad1f1d7be893c7f4c0ef6d40130daa6379311d89
alexsmn <alexsmn@gmail.com>
2018-11-15 16:44:31 -0700

    AddFavouritesDialog: New title was ignored.

--- components/favourites/views/add_favourites_dialog.cpp
+++ components/favourites/views/add_favourites_dialog.cpp
@@ -16,14 +16,12 @@ class AddFavouritesDialog : public framework::Dialog {
   explicit AddFavouritesDialog(Favourites& favourites)
       : Dialog{IDD_ADD_FAVOURITES}, favourites_{favourites} {}
 
-  void set_name(const base::string16& name) { name_ = name; }
-
-  const base::string16& name() const { return name_; }
-  const base::string16& folder() const { return folder_; }
+  base::string16 title;
+  base::string16 folder;
 
  protected:
   virtual void OnInitDialog() {
-    SetItemText(IDC_NAME, name_);
+    SetItemText(IDC_NAME, title);
 
     folder_combo_ = GetItem(IDC_FOLDER);
     for (auto& folder : favourites_.folders())
@@ -32,8 +30,8 @@ class AddFavouritesDialog : public framework::Dialog {
   }
 
   virtual void OnOK() {
-    name_ = GetItemText(IDC_NAME);
-    folder_ = win_util::GetWindowText(folder_combo_);
+    title = GetItemText(IDC_NAME);
+    folder = win_util::GetWindowText(folder_combo_);
 
     Dialog::OnOK();
   }
@@ -41,20 +39,18 @@ class AddFavouritesDialog : public framework::Dialog {
  private:
   Favourites& favourites_;
 
-  base::string16 name_;
-  base::string16 folder_;
-
   WTL::CComboBox folder_combo_;
 };
 
 bool ShowAddFavouritesDialog(DialogService& dialog_service,
                              AddFavouritesContext&& context) {
   AddFavouritesDialog dlg{context.favourites_};
-  dlg.set_name(context.window_def_.title);
+  dlg.title = context.window_def_.title;
   if (dlg.Execute(dialog_service.GetDialogOwningWindow()) != IDOK)
     return false;
 
-  const Page& folder = context.favourites_.GetOrAddFolder(dlg.folder().c_str());
+  const Page& folder = context.favourites_.GetOrAddFolder(dlg.folder.c_str());
+  context.window_def_.title = std::move(dlg.title);
   context.favourites_.Add(context.window_def_, folder);
   return true;
 }
