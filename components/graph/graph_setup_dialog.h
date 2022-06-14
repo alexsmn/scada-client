68625cc8b348734cdb941704b3846d0e23a07c42
alexsmn <alexsmn@gmail.com>
2022-06-13 17:02:48 -0700

    Build fix.

--- components/graph/graph_setup_dialog.h
+++ components/graph/graph_setup_dialog.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#include "controls/color.h"
 #include "common_resources.h"
+#include "controls/color.h"
 
 #include <algorithm>
 
@@ -10,9 +10,9 @@ using std::min;
 
 #include <atlbase.h>
 
-#include <wtl/atlapp.h>
-#include <wtl/atlctrls.h>
-#include <wtl/atlframe.h>
+#include <atlapp.h>
+#include <atlctrls.h>
+#include <atlframe.h>
 
 class GraphSetupDialog : public ATL::CDialogImpl<GraphSetupDialog>,
                          protected WTL::COwnerDraw<GraphSetupDialog> {
@@ -29,10 +29,10 @@ class GraphSetupDialog : public ATL::CDialogImpl<GraphSetupDialog>,
   void DrawItem(LPDRAWITEMSTRUCT /*lpDrawItemStruct*/);
 
   BEGIN_MSG_MAP(GraphSetupDialog)
-  MESSAGE_HANDLER(WM_INITDIALOG, OnInitDialog)
-  COMMAND_ID_HANDLER(IDOK, OnCloseCmd)
-  COMMAND_ID_HANDLER(IDCANCEL, OnCloseCmd)
-  CHAIN_MSG_MAP(WTL::COwnerDraw<GraphSetupDialog>)
+    MESSAGE_HANDLER(WM_INITDIALOG, OnInitDialog)
+    COMMAND_ID_HANDLER(IDOK, OnCloseCmd)
+    COMMAND_ID_HANDLER(IDCANCEL, OnCloseCmd)
+    CHAIN_MSG_MAP(WTL::COwnerDraw<GraphSetupDialog>)
   END_MSG_MAP()
 
   LRESULT OnInitDialog(UINT /*uMsg*/,
