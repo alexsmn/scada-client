6821df7f98bfac637605aa8d5a32fa2c53cc388e
alexsmn <alexsmn@gmail.com>
2023-11-15 19:55:05 -0800

    Remove component API.

--- controller/component_api_impl.cpp
+++
@@ -1,9 +0,0 @@
-#include "controller/component_api_impl.h"
-
-void ComponentApiImpl::RegisterControllerFactory(
-    const WindowInfo& window_info,
-    ControllerFactory controller_factory) {}
-
-SelectionCommands& ComponentApiImpl::GetSelectionCommands() {
-  return *selection_commands_;
-}
