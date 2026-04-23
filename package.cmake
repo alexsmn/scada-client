344d2007c9ce81a88d81ce0b9cd5c14b192eea93
alexsmn <alexsmn@gmail.com>
2026-04-22 17:45:19 -0700

    Add package distribution metadata

--- package.cmake
+++ package.cmake
@@ -5,6 +5,8 @@ scada_register_package(
   DISPLAY_NAME "Клиент"
   VERSION "${SCADA_PACKAGING_VERSION}"
   DESCRIPTION "Client package"
+  DISTRIBUTION_KIND "end-user"
+  STANDALONE_INSTALLER_DEFAULT ON
   INSTALL_DESTINATIONS "bin" "bin/translations" "examples/schemes"
   RUNTIME_DEPENDENCY_POLICY "repo-local-runtime-deps"
   ROOT_GROUPS product-client
